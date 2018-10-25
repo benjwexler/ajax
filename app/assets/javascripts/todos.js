b5723a4d8e197c72d66507c9e25919e5653b2cbe
Ben Wexler <mixedywex@gmail.com>
2018-10-25 01:12:14 -0400

    error handling for stock not listed

---
+++ app/assets/javascripts/todos.js
@@ -0,0 +1,104 @@
+let symbolsObj = {}
+
+$(function(){
+
+
+    
+
+    $.ajax({
+        dataType: 'jsonp',
+        // timeout: 5000, // sets timeout to 5 seconds
+        url: "https://api.iextrading.com/1.0/ref-data/symbols",
+        success: function(data){
+
+            // console.log(data[0].symbol)
+
+            for(let i=0; i<data.length; i++) {
+                // console.log(data[i].symbol)
+
+                symbolsObj[(data[i].symbol)] = 1
+            }
+         
+
+
+        }
+
+    })
+
+
+
+
+    $("form").submit(function(event){
+      event.preventDefault();
+  
+      var action = $(this).attr('action');
+      var method = $(this).attr('method');
+  
+    //   var description = $(this).find('#todo_description').val();
+    //   var priority = $(this).find('#todo_priority').val();
+
+          var stockSymbol = $(this).find('#todo_description').val();
+      var quantity = $(this).find('#todo_priority').val();
+
+    //   var totalPrice = $(this).find('#totalPrice')
+
+   
+
+      function ajaxCall2(){
+        $.ajax({
+        method: method,
+        url: action,
+        data: { todo: {description: fullPrice, priority: quantity} },
+       
+        // data: fullPrice,
+        dataType: 'script'
+           
+        });
+    }
+     
+    //   var data = $(this).serializeArray();
+        let fullPrice 
+
+        if(symbolsObj[`${stockSymbol.toUpperCase()}`] === 1) {
+      $.ajax({
+        // method: method,
+        // url: action,
+       
+        // data: data,
+        url: `https://api.iextrading.com/1.0/stock/${stockSymbol}/price`,
+        dataType: 'jsonp',
+        // timeout: 5000, // sets timeout to 5 seconds
+        
+        success: function(data){
+            // write your code here
+            
+                    console.log(data)
+
+                    console.log(data*quantity)
+
+
+                    fullPrice = data*quantity 
+              
+                    document.getElementById("totalPrice").value = fullPrice
+
+                    ajaxCall2(fullPrice);
+                    
+            },
+            error: function(xhr, status, error){
+                var errorMessage = xhr.status + ': ' + xhr.statusText
+                alert('Error - ' + errorMessage);
+            }
+
+            
+          // this line makes the response format JavaScript and not html.
+        // dataType: 'script'
+      });
+
+    } else {
+        alert("Fake Stock!")
+    }
+
+    
+  
+    });
+  });
---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,36 @@
+class TodosController < ApplicationController
+    def index
+      @todos = Todo.all
+
+
+      
+    end
+  
+    def create
+        # binding.pry
+    #   Todo.create(data)
+
+      @test = 5
+      respond_to do |format|
+        # if the response fomat is html, redirect as usual
+        format.html { 
+            p "hey"
+        redirect_to root_path }
+    
+        # if the response format is javascript, do something else...
+        format.js { p "ijfnr"}
+      end
+    end
+  
+    def destroy
+       
+      todo = Todo.find(params[:id])
+      todo.destroy
+      redirect_to root_path
+    end
+  
+    private
+      def todo_params
+        params.require(:todo).permit(:description, :priority)
+      end
+  end
